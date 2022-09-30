8c19bff54aba564cdd7d9e288f62bfa0a99ba1e4
Aquilesdevweb <aquilesnetto2002@gmail.com>
2022-09-30 11:33:51 -0300

    Commit keyup

---
+++ exercicio_html_js/form_ex/form.js
@@ -0,0 +1,12 @@
+function valida(){
+    const telefone = document.getElementById('telefone').value;
+    const cep = document.getElementById('cep').value;
+
+    console.log(telefone)
+
+    
+
+   // if (telefone < cep){
+    //    console.log
+    //})
+}
