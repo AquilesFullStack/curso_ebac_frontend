7bca9803675d4359c6645a612b52b1a1983eb2cb
José Aquiles <aquilesnetto2002@gmail.com>
2023-08-24 16:10:51 -0300

    exercicio_funcao

---
+++ FRONT-END/exmod26/main.js
@@ -0,0 +1,13 @@
+"use strict";
+function multiplica(numero1, numero2) {
+    return (numero1 * numero2);
+    //return  console.log(numero1 * numero2);
+}
+function RecebeNome(nome) {
+    return ("Olá" + nome);
+    //return console.log("Olá" + nome);
+}
+//multiplica(2,4);
+//RecebeNome(" João");
+console.log(multiplica(2, 4));
+console.log(RecebeNome(' Joao'));
---
+++ FRONT-END/exmod26/main.ts
@@ -0,0 +1,16 @@
+function multiplica(numero1 : number , numero2 : number){
+    return (numero1 * numero2);
+    //return  console.log(numero1 * numero2);
+}
+
+function RecebeNome(nome: string){
+    return ("Olá" + nome)
+    //return console.log("Olá" + nome);
+}
+
+//multiplica(2,4);
+//RecebeNome(" João");
+
+console.log(multiplica(2,4));
+console.log(RecebeNome(' Joao'));
+
